a0d251b784329d2077ccc780b5a7912a7e98796a
KUcodemaster <jh9485@korea.ac.kr>
2023-12-15 10:37:43 +0900

    feat: NotifyBar 개발

--- ollilove/src/pages/Index/Index.tsx
+++ ollilove/src/pages/Index/Index.tsx
@@ -3,11 +3,13 @@ import { H1, H2, H3, P1, P2, P3, P4, Comment } from "../../commons/Text";
 import { ButtonGray, ButtonYellow } from "../../commons/Button";
 import { Tabbar } from "../../commons/Tabbar";
 import { Navbar } from "../../commons/Navbar";
+import { NotifyBar } from "../../commons/NotifyBar";
 
 export default function Index() {
   return (
     <>
       <Navbar type="back">네비게이션 바</Navbar>
+      <NotifyBar>새로운 마음이 도착했어요!</NotifyBar>
       <H1>H1입니다</H1>
       <H2>H2입니다</H2>
       <H3>H3입니다</H3>
