3e07f85c3cd99744cfbaad18e7bc5462daf130af
gkgkgkgkgk73 <gkgkgkgkgk73@gmail.com>
2023-12-15 17:44:09 +0900

    add timer into videoRecorder component and fix css

--- ollilove/src/pages/TransferRecord/TransferRecord.tsx
+++ ollilove/src/pages/TransferRecord/TransferRecord.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import VideoRecorder from "../../components/VideoRecorder/VideoRecorder";
+import styled from "styled-components";
 
 export default function TransferConfirm() {
 
-    return (<VideoRecorder/>);
+    return (
+        <VideoRecorder/>
+    );
 
 }
